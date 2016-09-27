5c6c87a201f4cc50a911aad7022e69f826eb9cdf
mattiaS <mattia@3doers.it>
2016-09-27 12:46:44 +0100

    add db and env2 setup

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ const {
 } = require('@matthewglover/hapi-wrapper');
 
 
+const env = require('env2')('./.env');
 const connectionOptions = require('./options/connection');
 const configureHandlebars = require('./configure_handlebars');
 const { logStartup, logUnhandledError } = require('./loggers');
-
-
 const plugins = require('./plugins');
 const routes = require('./routes');
+
+require('monk')(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/worthwhile-charity`).then((db) => {
+  console.log(db._state);
+}).catch((err) => {
+  console.log(err);
+});
 
 
 createServer()
