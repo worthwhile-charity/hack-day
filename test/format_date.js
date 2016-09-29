9680ca522d167b85ada6889f0f362f71e78d6c20
Matt Glover <matthewglover@me.com>
2016-09-29 14:56:34 +0100

    update field names and format dates properly for display

--- test/format_date.js
+++ test/format_date.js
@@ -3,6 +3,6 @@ import moment from 'moment';
 
 const testDate = moment([2016, 8, 26]);
 
-test.only('moment formats dates', t => {
+test('moment formats dates', t => {
   t.is(testDate.format('DD-MM-YYYY'), '26-09-2016');
 });
