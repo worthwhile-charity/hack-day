af5c3177d36c0df4fe296f18cdde4a1b88a7534f
Matt Glover <matthewglover@me.com>
2016-10-02 09:00:17 +0100

    tidy up code

--- generate_data.js
+++ generate_data.js
@@ -1,12 +1,12 @@
 // Load any environment variables first
 require('env2')('./.env');
-// const createJob = require('./lib/jobs/create_job');
+
 const Job = require('./lib/job');
-const fakeJobs = require('./lib/fake_jobs')(100); // number of fake jobs
+const genFakeJobs = require('./lib/gen_fake_jobs');
 const dbConnect = require('./lib/db_connect');
 
 dbConnect();
 
-Job.create(fakeJobs)
+Job.create(genFakeJobs(100))
   .then(res => console.log('SUCCESS')) // eslint-disable-line
   .catch(err => console.log('ERROR')); // eslint-disable-line
